cb248198791423734c93027bc05716e50bb9a9b5
elafatal <vahediielahe@gmail.com>
2024-05-16 00:33:38 +0330

    Completing the process of the two-player game

--- script/phase2.js
+++ script/phase2.js
@@ -11,13 +11,13 @@ window.addEventListener("keydown", function (e) {
 
     switch (e.key) {
         case "ArrowLeft":
-          player2_move="paper";   
+          player2_move="paper";  
         break;
         case "ArrowUp":
           player2_move="scissor";
           break;
         case "ArrowRight":
-          player2_move="rock";
+          player2_move="rock"; 
           break;
         case "a":
           player1_move="paper";
@@ -30,6 +30,58 @@ window.addEventListener("keydown", function (e) {
           break;
       }
 
+          
+    if (player1_move!= "" && player2_move!=""){
+      if(player1_move==="paper"){
+          if(player2_move==="paper"){
+              Result.innerText="DRAW";
+              Result.classList="text-warning";
+              player2_score++;
+              player1_score++;
+          } else if(player2_move==="scissor"){
+              Result.innerText="PLAYER 2 WIN";
+              Result.classList="text-danger";
+              player2_score++;
+          } else if(player2_move==="rock"){
+              Result.innerText="PLAYER 1 WIN";
+              Result.classList="text-info";
+              player1_score++;
+          }
+      } else if (player1_move==="scissor") {
+          if(player2_move==="scissor"){
+              Result.innerText="DRAW";
+              Result.classList="text-warning";
+              player2_score++;
+              player1_score++;
+          } else if(player2_move==="rock"){
+              Result.innerText="PLAYER 2 WIN";
+              Result.classList="text-danger";
+              player2_score++;
+          } else if(player2_move==="paper"){
+              Result.innerText="PLAYER 1 WIN";
+              Result.classList="text-info";
+              player1_score++;
+          }
+      } else if (player1_move=="rock"){
+          if(player2_move==="rock"){
+              Result.innerText="DRAW";
+              Result.classList="text-warning";
+              player2_score++;
+              player1_score++;
+          } else if(player2_move==="paper"){
+              Result.innerText="PLAYER 2 WIN";
+              Result.classList="text-danger";
+              player2_score++;
+          } else if(player2_move==="scissor"){
+              Result.innerText="PLAYER 1 WIN";
+              Result.classList="text-info";
+              player1_score++;
+          }
+      }
+      score.innerText=`Player1 --- ${player1_score} :  ${player2_score} --- player2`;
+      player1= 0;
+      player2=0;
+  }
       
-})
+}, true)
 
