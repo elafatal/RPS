edb43926075a76fdf5b7d9c741970ac882512d11
elafatal <vahediielahe@gmail.com>
2024-05-16 01:02:41 +0330

    fixed bugs

--- script/phase2.js
+++ script/phase2.js
@@ -30,57 +30,56 @@ window.addEventListener("keydown", function (e) {
           break;
       }
 
-          
     if (player1_move!= "" && player2_move!=""){
       if(player1_move==="paper"){
           if(player2_move==="paper"){
               Result.innerText="DRAW";
-              Result.classList="text-warning";
+              Result.classList="text-center mt-5 pt-3 text-warning"  ;
               player2_score++;
               player1_score++;
           } else if(player2_move==="scissor"){
               Result.innerText="PLAYER 2 WIN";
-              Result.classList="text-danger";
+              Result.classList="text-center mt-5 pt-3 text-danger"  ;
               player2_score++;
           } else if(player2_move==="rock"){
               Result.innerText="PLAYER 1 WIN";
-              Result.classList="text-info";
+              Result.classList="text-center mt-5 pt-3 text-info"  ;
               player1_score++;
           }
       } else if (player1_move==="scissor") {
           if(player2_move==="scissor"){
               Result.innerText="DRAW";
-              Result.classList="text-warning";
+              Result.classList="text-center mt-5 pt-3 text-warning"  ;
               player2_score++;
               player1_score++;
           } else if(player2_move==="rock"){
               Result.innerText="PLAYER 2 WIN";
-              Result.classList="text-danger";
+              Result.classList="text-center mt-5 pt-3 text-danger"  ;
               player2_score++;
           } else if(player2_move==="paper"){
               Result.innerText="PLAYER 1 WIN";
-              Result.classList="text-info";
+              Result.classList="text-center mt-5 pt-3 text-info"  ;
               player1_score++;
           }
       } else if (player1_move=="rock"){
           if(player2_move==="rock"){
               Result.innerText="DRAW";
-              Result.classList="text-warning";
+              Result.classList="text-center mt-5 pt-3 text-warning"  ;
               player2_score++;
               player1_score++;
           } else if(player2_move==="paper"){
               Result.innerText="PLAYER 2 WIN";
-              Result.classList="text-danger";
+              Result.classList="text-center mt-5 pt-3 text-danger"  ;
               player2_score++;
           } else if(player2_move==="scissor"){
               Result.innerText="PLAYER 1 WIN";
-              Result.classList="text-info";
+              Result.classList="text-center mt-5 pt-3 text-white text-info"  ;
               player1_score++;
           }
       }
       score.innerText=`Player1 --- ${player1_score} :  ${player2_score} --- player2`;
-      player1= 0;
-      player2=0;
+      player1_move= "";
+      player2_move="";
   }
       
 }, true)
