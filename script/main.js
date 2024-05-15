fd5f2a53961d1d43a079803a7ed0b5e881a84030
elafatal <vahediielahe@gmail.com>
2024-05-15 21:09:37 +0330

    finish game

--- script/main.js
+++ script/main.js
@@ -6,27 +6,89 @@ function getRandomNumber() {
 
 let player_score=0;
 let computer_score=0; 
+const player = document.getElementById("player");
+const Result = document.getElementById("Result");
 
 // player
 const paper= document.getElementById("paper"); 
 const scissor= document.getElementById("scissor"); 
 const rock= document.getElementById("rock");
-// pc
 
+// pc
 const randomNumber=getRandomNumber();
 const pc=document.getElementById("pc-choosed")
 
 
-if (randomNumber<1 && randomNumber<4){
-    switch(randomNumber) {
-        case 1:
-            pc.innerText="Computer chose : Paper";
-        break;
-        case 2:
-            pc.innerText="Computer chose : Scissor";
-        break;
-        case 3:
-            pc.innerText="Computer chose : Rock";
-        break;
-    }
-}
+
+    player.addEventListener("click",(e)=>{
+        e.preventDefault();
+        if (paper.checked || rock.checked || scissor.checked){
+            switch(randomNumber) {
+                case 1:
+                    pc.innerText="Computer chose : Paper";
+                    player.innerText="PLAY AGAIN"
+                break;
+                case 2:
+                    pc.innerText="Computer chose : Scissor";
+                    player.innerText="PLAY AGAIN"
+                break;
+                case 3:
+                    pc.innerText="Computer chose : Rock";
+                    player.innerText="PLAY AGAIN"
+                break;
+            }
+        }
+        
+        //result
+        if(paper.checked){
+            if(randomNumber==1){
+                Result.innerText="DRAW";
+                Result.classList="text-center text-white mt-5 pt-5 text-warning";
+                computer_score++;
+                player_score++;
+            } else if(randomNumber==2){
+                Result.innerText="YOU LOST !";
+                Result.classList=" text-center text-white mt-5 pt-5 text-danger";
+                computer_score++;
+            } else if(randomNumber==3){
+                Result.innerText="YOU WIN !";
+                Result.classList="text-center text-white mt-5 pt-5 text-success";
+                player_score++;
+            }
+        } else if (scissor.checked) {
+            if(randomNumber==1){
+                Result.innerText="YOU WIN !";
+                Result.classList="text-center text-white mt-5 pt-5 text-success";
+                player_score++;
+            }else if(randomNumber==2){
+                Result.innerText="DRAW";
+                Result.classList="text-center text-white mt-5 pt-5 text-warning";
+                computer_score++;
+                player_score++;
+            } else if(randomNumber==3){
+                Result.innerText="YOU LOST !";
+                Result.classList="text-center text-white mt-5 pt-5text-danger";
+                computer_score++;
+            }
+            }else if (rock.checked){
+                if(randomNumber==1){
+                    Result.innerText="YOU LOST !";
+                    Result.classList="text-center text-white mt-5 pt-5 text-danger";
+                    computer_score++;
+                } else if(randomNumber==2){
+                    Result.innerText="YOU WIN !";
+                    Result.classList="text-center text-white mt-5 pt-5 text-success";
+                    player_score++;
+                }else  if(randomNumber==3){
+                    Result.innerText="DRAW";
+                    Result.classList="text-center text-white mt-5 pt-5 text-warning";
+                    computer_score++;
+                    player_score++;
+                }
+            } 
+            score.innerText=`Computer: ${computer_score} | Player: ${player_score}`;
+            rock.checked=false;
+            scissor.checked=false;
+            paper.checked=false;
+
+    } )
